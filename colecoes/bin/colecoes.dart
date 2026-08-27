6c2766e1657d289fe8ee3aab70892fdfa4fdd05a
mamazzei-edu <89044909+mamazzei-edu@users.noreply.github.com>
2026-08-27 13:27:50 -0300

    Aula de 27/08:

--- colecoes/bin/colecoes.dart
+++ colecoes/bin/colecoes.dart
@@ -1,90 +1,56 @@
 import 'package:colecoes/colecoes.dart' as colecoes;
 
 void main(List<String> arguments) {
-  final nomes = ["João", "Maria", "José", "Ana", "Carlos"];
-  // nomes = ['Carlos', 'Lucas'];
-  nomes[0] = "Cristina";
-  print(nomes);
-  const nomesConstantes = ["João", "Maria", "José", "Ana", "Carlos"];
-  nomesConstantes[0] = "Cristina";
-  print(nomesConstantes);
-  // int soma = 0;
-  // for (final arg in arguments) {
-  //   print(arg);
-  //   print(arg.runtimeType);
-  //   int? numero = int.tryParse(arg);
-  //   if (numero != null) {
-  //     print("O número é: $numero");
-  //     soma += numero;
-  //   }
-  // }
-  // print("A soma dos números é: $soma");
-  // print(arguments.isEmpty);
-  // print(arguments.isNotEmpty);
-  // var nomes = ['João', 'Maria', 'José', 'Ana', 'Carlos'];
-  // nomes.add("Cristina");
-  // print(nomes);
-  // nomes.insert(0, "Ana Maria");
-  // print(nomes);
-  // nomes.insert(5, "Vagner");
-  // print(nomes);
-  // nomes.insert(7, "Lucas");
-  // print(nomes);
-  // print(nomes.contains("Lucas"));
-  // print(nomes.contains("LUCAS"));
-  // List<String> nomesSemInfericao = ["Cássio", "Bruna", "Diego"];
-  // print(nomesSemInfericao);
-  // print(nomesSemInfericao.runtimeType);
-  // List<int> idadesSemAfericao = [13, 15, 20];
-  // print(idadesSemAfericao);
-  // print(idadesSemAfericao.runtimeType);
-  // List<bool> maioridadeSemInfericao = [false, false, true];
-  // print(maioridadeSemInfericao);
-  // print(maioridadeSemInfericao.runtimeType);
-  // var listaDeListas = [
-  //   nomesSemInfericao,
-  //   idadesSemAfericao,
-  //   maioridadeSemInfericao,
-  // ];
-  // print(listaDeListas);
-  // List lista = [];
-  // lista.add("João");
-  // lista.add(15);
-  // lista.add(true);
-  // print(lista);
-  // print(lista.runtimeType);
-  // lista[0] = 2;
-  // print(lista);
-  // print(lista.runtimeType);
-  // var somenteStrings = <String>['Ana', 'Pedro'];
-  // print(somenteStrings);
-  // print(somenteStrings.runtimeType);
-  //print(nomes.reversed);
-  // // var numeros = [1, 2, 3, 4, 5];
-  // print(nomes);
-  // print(nomes.first);
+  List lista = [];
+  lista.add("Ana");
+  lista.add(false);
+  print(lista.runtimeType);
+  print(lista);
+  // List<String> nomes = ['Alice', 'Bob', 'Charlie', 'João', 'Pedro', 'Maria'];
+  // List<int> numeros = [1, 2, 3, 4, 5];
+  // List<bool> booleanos = [true, false, true];
+  // var listas = [nomes, numeros, booleanos];
+  // print(listas);
+  // print(listas.runtimeType);
+
+  //   var nomes = ['Alice', 'Bob', 'Charlie','João','Pedro','Maria'];
+  //     nomes.add("Cristina");
+  //     nomes.insert(arguments.length, "Daniel");
+  // //    nomes.insert(200,"Carlos");
+  //     print(nomes);
+  //     print(nomes.contains("João"));
+  //     print(nomes.contains("Joao"));
+  //     print(nomes.contains("joão"));
+  // print(nomes.isEmpty);
+  // print(nomes.isNotEmpty);
+  // print(nomes.reversed);
+  // List<String> listaInvertida = nomes.reversed.toList();
+  // print(listaInvertida);
   // var vazia = [];
-  // print(vazia.isEmpty);
-  // //print(vazia.first);
+  // // print(vazia.first);
   // print(vazia.firstOrNull);
-  // print([].firstOrNull);
-  // print(nomes.last);
-  // print(vazia.lastOrNull);
+  // // print(colecoes.soma(arguments));
+  // var nomes = ['João', 'Pedro', 'Maria'];
+  // var numeros = [1, 2, 3];
+  // print(nomes);
   // print(nomes.toString());
   // print(nomes.runtimeType);
-  // print(numeros.toString());
+  // print(numeros);
   // print(numeros.runtimeType);
-  // print(nomes[0]);
-  // //  nomes[0] = "José";
-  // //  print(nomes);
+  // //  print(nomes[-1]);
+  // //  print(nomes[3]);
+  // nomes[0] = 'José';
+  // print(nomes);
+  // print("Primeira forma de percorrer a lista");
   // for (int i = 0; i < nomes.length; i++) {
   //   print(nomes[i]);
   // }
-  // print("Inicianco segunda forma de percorrer a lista");
+  // print("Segunda forma de percorrer a lista");
   // for (final nome in nomes) {
   //   print(nome);
   // }
-  // var tiposVariados = ["Ana", true, 2, 2.5];
-  // print(tiposVariados);
-  // print(tiposVariados.runtimeType);
+  // var itensDiversos = ['João', 1, true, 2.5];
+  // print(itensDiversos);
+  // print(itensDiversos.runtimeType);
+  // itensDiversos[0]=10;
 }
--- colecoes/lib/colecoes.dart
+++ colecoes/lib/colecoes.dart
@@ -1,3 +1,11 @@
 int calculate() {
   return 6 * 7;
 }
+
+int soma(List<String> arguments) {
+  var soma = 0;
+  for (String valorStr in arguments) {
+    soma += int.tryParse(valorStr) ?? 0;
+  }
+  return soma;
+}
