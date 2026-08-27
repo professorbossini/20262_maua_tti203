ef99c3e40cb31756b5ed83b62ff3a5d77bce3260
Rodrigo Bossini <professorbossini@gmail.com>
2026-08-27 12:09:34 -0300

    quinta(ap03): ex pg 11 proposto

---
+++ quinta_bossini/colecoes/lib/colecoes.dart
@@ -0,0 +1,43 @@
+void testes(){
+  //união, intersecção, diferença
+  var A = {1, 2, 3, 4, 5, 6};
+  var B = {1, 3, 7};
+  print(A.union(B));
+  print(A.intersection(B));
+  print(A.difference(B));
+  print(B.difference(A));
+  // const nomes = {'Ana', 'João'};
+  // print(nomes.elementAt(0));
+  // for(const nome in nomes){
+  //   print(nome);
+  // }
+  // {}: sets e maps
+  // var paises = <String, int> {};
+  // var nomes = {'Ana', 'João'};
+  // print(nomes);
+  // var paises = {'Brasil', 'Brasil'};
+  // print(paises);
+  // var teste1 = {};
+  //tuplas
+  // var tupla = ('Ana', 18, true, false);
+  // print(tupla.$1);
+  // print(tupla.$2);
+  // print(tupla.$3);
+  // print(tupla.$4);
+  //type annotation
+  // List<int> lista = [];
+  // lista.
+  // var l3 = <String?> [null];
+  // List<String?>? l2 = ["Ana", null];
+  // l2 = null;
+  // List <String> l1 = ["Ana", 1];
+  // var vazia = [];
+  //var nomes = ["Ana", "João"];
+  //nomes.add(null);
+  // const nomes = ["Ana", "Pedro"];
+  // // nomes = ["A"];
+  // nomes[0] = "A";
+  // final nomes = [1.1, 1, "Ana"];
+  // nomes[2] = "Ana Silva";
+  // nomes = ["Outra"];
+}
