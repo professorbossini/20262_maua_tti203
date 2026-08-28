d922cdceb880718e60b2788a735e48d9c199a87e
Rodrigo Bossini <professorbossini@gmail.com>
2026-08-28 08:58:50 -0300

    sexta(ap03): ex pg 11 proposto

---
+++ sexta_7h40_bossini/colecoes/lib/colecoes.dart
@@ -0,0 +1,72 @@
+void testes(){
+  var portugues = {"Brasil", "Portugal"};
+  var europa = {"Alemanha", "Portugal", "Espanha"};
+  //todos os países em que se fala português e todos os países da Europa
+  //Todos os países em que se fala português e que são Europeus
+  //Todos os países em que se fala português e que não são europeus
+  //Todos os países exceto aqueles em que se fala português e que são europeus(simultaneamente)
+  //não use variáveis outras
+  print();
+  print();
+  print();
+  print();
+
+
+
+  // var A = {1, 2, 3, 4, 5, 6};
+  // var B = {1, 3, 7};
+  // //união, intersecção, diferença
+  // print(A.union(B));
+  // print(B.union(A));
+  // print(A.intersection(B));
+  // print(B.intersection(A));
+  // print(A.difference(B));
+  // print(B.difference(A));
+  // // var nomes = {"Ana", "João"};
+  // //print(nomes[0]);
+  // // print(nomes.elementAt(0));
+  // for(const nome in nomes){
+  //   print(nome);
+  //   // nome = "ANa";
+  // }
+  // var teste1 = {};
+  // var teste2 = <String>{};
+  // var teste3 = <int, bool>{};
+  //set: conjunto
+  //{}: set e map
+  //var nomes = {};
+  // var nomes = {"Ana", "João"};
+  // var paises = {"Brasil", "Brasil"};
+  // print(nomes);
+  // print(paises);
+  //tuplas
+  // var tupla = ('Ana', 18, true, 'Rua B');
+  // print(tupla);
+  // print(tupla.runtimeType);
+  // print(tupla.$1);
+  // print(tupla.$2);
+  // print(tupla.$3);
+  // print(tupla.$4);
+
+
+  // var numeros = [1, 1.1];
+  // var whatever = ["A", 1, true, null];
+  // var eAgora2 = [null];
+  //type annotation
+  // var lista = <int?> [1];
+  // List<String?>? eAgora;
+  // var letras = ["A", "B"];
+  // letras.add("C");
+  // // letras.add(null);
+  // letras = null;
+
+  // const letras = ["A", "B"];
+  // letras.add("C");
+  // letras[0] = "C";
+  //letras = ["J"];
+  // final nomes = ["A", "B"];
+  // nomes[0] = "C";
+  // nomes.add("a");
+  // nomes = ["J"];
+
+}
