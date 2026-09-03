a38db2b953da88777fdfbdf83fc22514120bd4b0
mamazzei-edu <89044909+mamazzei-edu@users.noreply.github.com>
2026-09-03 11:04:07 -0300

    Aula de 03/09/2026

---
+++ exibe_imagens/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var app = const MaterialApp(
+    home: Scaffold(body: Center(child: Text('Olaaaaaá, mundo vasto com flutter!'))),
+  );
+  runApp(app);
+}
