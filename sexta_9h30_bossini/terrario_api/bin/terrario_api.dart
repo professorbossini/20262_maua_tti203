c7ebef6c8d82eea7bc9a567487b694c677c23d21
Rodrigo Bossini <professorbossini@gmail.com>
2026-09-11 11:04:39 -0300

    sexta-09h30(ap04): lida com o codigo 3.5

---
+++ sexta_9h30_bossini/terrario_api/bin/terrario_api.dart
@@ -0,0 +1,5 @@
+// import 'package:terrario_api/terrario_api.dart' as terrario_api;
+
+void main(List<String> arguments) {
+  
+}
---
+++ sexta_9h30_bossini/terrario_api/lib/src/config/app_config.dart
@@ -0,0 +1,12 @@
+import 'env.dart';
+class AppConfig{
+  final String appEnv;
+  final int serverPort;
+  final String logLevel;
+  final String dbHost;
+  final int dbPort;
+  final String dbName;
+  final String dbUser;
+  final String dbPassword;
+  final int dbPoolSize;
+}
